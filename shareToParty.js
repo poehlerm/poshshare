069acecb27e6f4a66ea5f98eb8bfdf0372eacd88
Jordan <jordanmoconnor@gmail.com>
2016-06-09 22:17:06 -0400

    Update shareToParty.js

--- shareToParty.js
+++ shareToParty.js
@@ -1,24 +1,25 @@
 //Share all items to party
 function shareAllItems(){
   var listing = document.querySelectorAll('.listing-con');
-
+  var lastSellingItem = 0;
   //find the first sold item
   for(var i = 0; i < listing.length; i++){
+    lastSellingItem = i;
     if(listing[i].querySelector('.sprite.sold-tag'))
     {
       console.log("Item number %d has been sold", i);
-      var lastSellingItem = i - 1;
+      lastSellingItem = i - 1;
       i = listing.length;
     }
   }
 
   var buttons = document.querySelectorAll('.sprite.share');
-  var sharebutton = document.querySelector(".ib.va-m");
+  // var sharebutton = document.querySelector(".ib.va-m");
   //share all items before the sold items
   for(var i = lastSellingItem; i >= 0; i--){
     buttons[i].click();
-    var sharebutton = document.querySelectorAll(".ib.va-m");
-    sharebutton[1].click();
+    var sharebutton = document.querySelector(".sprite.btn-pm-party-share");
+    sharebutton.click();
     //sharebutton.click();
     console.log("Item %d was shared", i);
     }
