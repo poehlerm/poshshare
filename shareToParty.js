5ce67758dda7009107155ddff800444a2d90949d
Jordan <jordanmoconnor@gmail.com>
2016-10-05 18:12:30 -0400

    Update shareToParty.js

--- shareToParty.js
+++ shareToParty.js
@@ -1,27 +1,19 @@
 //Share all items to party
-function shareAllItems(){
-  var listing = document.querySelectorAll('.listing-con');
-  var lastSellingItem = 0;
-  //find the first sold item
-  for(var i = 0; i < listing.length; i++){
-    lastSellingItem = i;
-    if(listing[i].querySelector('.sprite.sold-tag'))
-    {
-      console.log("Item number %d has been sold", i);
-      lastSellingItem = i - 1;
-      i = listing.length;
-    }
-  }
+var listing = document.querySelectorAll('.listing-con');
+var buttons = document.querySelectorAll('a.share');
+var i = 0;
 
-  var buttons = document.querySelectorAll('.sprite.share');
-  // var sharebutton = document.querySelector(".ib.va-m");
-  //share all items before the sold items
-  for(var i = lastSellingItem; i >= 0; i--){
-    buttons[i].click();
-    var sharebutton = document.querySelector(".sprite.btn-pm-party-share");
+//share all available items
+function shareAllItems() {
+  setTimeout(function () {
+    buttons[buttons.length - 1 - i].click();
+    var sharebutton = document.querySelector(".btn-pm-party-share");
     sharebutton.click();
-    //sharebutton.click();
-    console.log("Item %d was shared", i);
+    i++;
+    if(buttons.length - i > 0) {
+      shareAllItems();
     }
+  }, 500)
 }
-shareAllItems();
+
+shareAllItems(); 
