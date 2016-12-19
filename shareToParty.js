f2750247fa3584f17acfe61d746d1ae6f8ccc44d
Jordan <jordanmoconnor@gmail.com>
2016-12-19 15:09:45 -0500

    Update shareToParty.js

--- shareToParty.js
+++ shareToParty.js
@@ -1,13 +1,13 @@
-//Share all items to party
-var listing = document.querySelectorAll('.listing-con');
-var buttons = document.querySelectorAll('a.share');
+//Share all items to followers
+var listing = document.querySelectorAll('.tile');
+var buttons = document.querySelectorAll('.share-gray');
 var i = 0;
 
 //share all available items
 function shareAllItems() {
   setTimeout(function () {
     buttons[buttons.length - 1 - i].click();
-    var sharebutton = document.querySelector(".btn-pm-party-share");
+    var sharebutton = document.querySelector(".pm-party-share-link");
     sharebutton.click();
     i++;
     if(buttons.length - i > 0) {
